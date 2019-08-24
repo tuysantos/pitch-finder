b64a8c8dfdc3c6e58ca678ee62efdbdd004be138
Artur Santos <artur_santos@hotmail.com>
2019-08-24 23:14:00 +0100

    code cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   { path: "", redirectTo: "/pitches", pathMatch: "full" }
-  //{ path: "**", redirectTo: "/pitches" }
 ];
 
 @NgModule({
